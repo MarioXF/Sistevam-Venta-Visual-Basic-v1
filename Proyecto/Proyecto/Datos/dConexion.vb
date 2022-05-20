3a8c9fafe9db0c3a375145367c1137a64e95bb8a
MarioXF <73730620+MarioXF@users.noreply.github.com>
2022-05-20 15:38:08 -0500

    Proyecto Ventas

---
+++ Proyecto/Proyecto/Datos/dConexion.vb
@@ -0,0 +1,23 @@
+﻿Imports System.Data.SqlClient
+Imports System.Configuration
+Public Class dConexion
+
+    Public con As SqlConnection = New SqlConnection(" data source=SHUMAQRUNAPC; initial catalog=datosventas; user id=sa; password=rupay968360732")
+    Public Sub conectar()
+        Try
+            con.Open()
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+    End Sub
+    Public Sub desconectar()
+        Try
+            If con.State = ConnectionState.Open Then
+                con.Close()
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+End Class
